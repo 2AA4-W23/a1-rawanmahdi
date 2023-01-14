091756bb7c9df0f094c61ca623e1301ab2d765c6
rawanmahdi <mahdir3@mcmaster.ca>
2023-01-14 18:27:09 -0500

    Updated backlog dates

--- src/main/java/pk/Dice.java
+++ src/main/java/pk/Dice.java
@@ -6,8 +6,8 @@ public class Dice {
 
     public Faces roll() {
         int howManyFaces = Faces.values().length;
-        System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
-        System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
+        //System.out.println("  (DEBUG) there are " + howManyFaces + " faces");
+        //System.out.println("  (DEBUG) " + Arrays.toString(Faces.values()));
         Random bag = new Random();
         return Faces.values()[bag.nextInt(howManyFaces)];
     }
@@ -20,5 +20,4 @@ public class Dice {
         }
         return faces;
     }
-
 }
