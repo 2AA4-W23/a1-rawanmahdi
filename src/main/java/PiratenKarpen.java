e1f6b5b5821f112df1c5fd4e02867292f1dcafdb
rawanmahdi <mahdir3@mcmaster.ca>
2023-01-17 13:38:37 -0500

    Delivered F03

--- src/main/java/PiratenKarpen.java
+++ src/main/java/PiratenKarpen.java
@@ -4,6 +4,10 @@ public class PiratenKarpen {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Piraten Karpen Simulator!");
+        for(int i=1; i<43; i++){
+            System.out.println("Starting game ", i, " of 42");
+            
+        }
         System.out.println("I'm rolling a dice");
         Dice myDice = new Dice();
         System.out.println(myDice.roll());
